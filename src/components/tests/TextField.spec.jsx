afb5148a18def26b6dc6dcf58e0551c52daadf50
whewchews <whewchews@gmail.com>
2024-03-04 01:15:38 +0900

    Test: Text field placeholder matcher

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -21,7 +21,32 @@ it('className prop으로 설정한  css class가 적용된다.', async () => {
   // className이란 내부 prop이나 state 값을 검증 (X)
   // 렌더링 되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
   await render(<TextField className="my-class" />);
+
   expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
     'my-class',
   );
+});
+
+// it -> test 함수의  alias
+// it'('should ~~~~')
+// test'('if ~~~~')
+
+describe('placeholder', () => {
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    //  기대결과 === 기대 결과 -> 성공
+    // 기대결과 !== 실제 결과 -> 실패
+    await render(<TextField className="my-class" />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    screen.debug();
+    expect(textInput).toBeInTheDocument();
+    // 단언(assertion) -> 테스트 통과하기 위한 조건 -> 검증 실행
+    // matcher:  기대 결과를 검증하기 위해 사용하는 일종의 api 집합
+  });
+
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    await render(<TextField placeholder="상품명을 입력해 주세요." />);
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+    expect(textInput).toBeInTheDocument();
+  });
 });
